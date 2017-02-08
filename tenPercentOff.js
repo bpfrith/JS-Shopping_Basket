35fe698e15e7de58eb3243bd6baf1733cc8bfd84
Ben Frith <bpfrith@gmail.com>
2017-02-08 01:01:05 +0000

    it works

--- tenPercentOff.js
+++ tenPercentOff.js
@@ -1,17 +1,17 @@
-// tenPercentOff = {
-//   discount: function(basket){
-//     if(basket.total === undefined){
-//       var subtotal = basket.calculateTotal();
-//     } else {
-//       var subtotal = basket.total;
-//     }
-//
-//     if(subtotal > 20){
-//       basket.finalTotal = (subtotal * 0.9).toFixed(2);
-//     } else {
-//       basket.finalTotal = subtotal;
-//     }
-//   }
-// }
-//
-// module.exports = tenPercentOff
+tenPercentOff = {
+  discount: function(basket){
+    if(basket.total === undefined){
+      var subtotal = basket.calculateTotal();
+    } else {
+      var subtotal = basket.total;
+    }
+
+    if(subtotal > 20){
+      basket.total = (subtotal * 0.9).toFixed(2);
+    } else {
+      basket.total = subtotal;
+    }
+  }
+}
+
+module.exports = tenPercentOff
